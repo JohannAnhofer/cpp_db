ab3a00e8bdc2f99e1b83183ac98c0e79daa51bad
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-09-12 10:30:32 +0200

    Initial checkin

---
+++ cppdb.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-09-11T16:57:55
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = cppdb
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS += \
+    nullable.h \
+    value_is_null.h \
+    nullable_types.h
---
+++ nullable_types.h
@@ -0,0 +1,49 @@
+#ifndef CPP_DB_NULLABLE_TYPES_H
+#define CPP_DB_NULLABLE_TYPES_H
+
+#include "nullable.h"
+#include <string>
+
+namespace cpp_db
+{
+    // char types
+    typedef nullable<char>          nullable_char;
+    typedef nullable<unsigned char> nullable_uchar;
+    typedef nullable<signed char>   nullable_schar;
+    typedef nullable<wchar_t>       nullable_wchar_t;
+
+    // signed integer types
+    typedef nullable<short>     nullable_short;
+    typedef nullable<int>       nullable_int;
+    typedef nullable<long>      nullable_long;
+    typedef nullable<long long> nullable_long_long;
+
+    // unsigned integer types
+    typedef nullable<unsigned short>     nullable_ushort;
+    typedef nullable<unsigned int>       nullable_uint;
+    typedef nullable<unsigned long>      nullable_ulong;
+    typedef nullable<unsigned long long> nullable_ulong_long;
+
+    // floating point types
+    typedef nullable<float>         nullable_float;
+    typedef nullable<double>        nullable_double;
+    typedef nullable<long double>   nullable_long_double;
+
+    // string types
+    typedef nullable<std::string>   nullable_string;
+    typedef nullable<std::wstring>  nullable_wstring;
+
+    // date/time types
+
+    // NULL
+    template<>
+    class nullable<void>
+    {
+    public:
+        bool is_null() const {return true;}
+    };
+
+    typedef nullable<void> null_type;
+}
+
+#endif // CPP_DB_NULLABLE_TYPES_H
