642e2311ae5e608a4b5ed84622be319eb682de2d
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-09-20 10:25:00 +0200

    Add a simple testframework

--- cpp_db/main.cpp
+++ cpp_db/main.cpp
@@ -1,10 +1,11 @@
-#include "nullable.h"
 #include "nullable_types.h"
 #include "key_value_pair.h"
 #include "coalesce.h"
+#include "test.h"
 
 #include <iostream>
 #include <memory>
+#include <functional>
 
 std::ostream &operator<<(std::ostream &out, cpp_db::null_type)
 {
@@ -16,65 +17,80 @@ int main(int , char *[])
 {
     std::cout << std::endl;
 
+	std::cout << "---------- Test testfunctions ----------" << std::endl;
+	test_condition("TRUE-Test success", true);
+	test_condition("TRUE-Test failed", false);
+	test_condition("EXCEPTIONS-Test", []() {if (false) return true; else throw std::runtime_error("This is a test!"); });
+	TEST_VERIFY([]() {if (false) return true; else throw std::runtime_error("This is a test!"); });
+	TEST_EQUAL(3, 4711);
+	TEST_EQUAL(4, 4);
+	TEST_NOT_EQUAL(3, 4711);
+	TEST_NOT_EQUAL(4, 4);
+
+	std::cout << "========== Test starts ==========" << std::endl;
+
     try
     {
         cpp_db::nullable_int a, c(815);
 
         a = 4711;
 
-        if (a==13)
-            std::cout << "Strange!!!\n";
-        else
-            std::cout << "Equal!!!\n";
-
         int b = a;
         cpp_db::nullable_int d;
         const cpp_db::nullable_int e(10), f;
 
-        std::cout << "b=" << b << ", c=" << c << std::endl;
-        std::cout << std::boolalpha
-                  << "a is null: "      << cpp_db::is_null(a) << " " << a.is_null()
-                  << "\nd is null: "    << cpp_db::is_null(d) << " " << d.is_null()
-                  << "\ne is null: "    << cpp_db::is_null(e) << " " << e.is_null()
-                  << "\nf is null: "    << cpp_db::is_null(f) << " " << f.is_null()
-                  << "\nnull is null: " << cpp_db::is_null(cpp_db::nullable_int()) << " " << cpp_db::nullable_int().is_null()
-                  << std::endl;
-		{
-            using namespace cpp_db;
-			null_type null;
+		std::cout << "---------- Test nullable ----------" << std::endl;
 
-            coalesce_trait<int, double, float>::type x0(0);
-            coalesce_trait<null_type, double, float>::type x1(0.0);
-            coalesce_trait<int, null_type, float>::type x2(0);
-            coalesce_trait<null_type, null_type, float>::type x3(0.0f);
-            coalesce_trait<int, double, null_type>::type x4(0);
-            coalesce_trait<null_type, double, null_type>::type x5(0.0f);
-            coalesce_trait<int, null_type, null_type>::type x6(0);
-			coalesce_trait<null_type, null_type, null_type>::type x7;
+		TEST_NOT_EQUAL(a, 13);
+		TEST_EQUAL(a, 4711);
+		TEST_EQUAL(b, a);
+		TEST_EQUAL(b, 4711);
+		TEST_EQUAL(c, 815);
 
-			std::cout << "Coalesce: \n-No nulls: " << coalesce(1, 2, 3, 4)
-                << "\n-First null, no other null: " << coalesce(null_type(), 2, 3, 4)
-				<< "\n-Only nulls: " << coalesce(null, null, null)
-				<< "\n-First non null: " << coalesce(1, null, null, null)
-                << "\n-First Null: " << coalesce(null, 2, null, 4)
-                << "\n* x0==0 " << std::boolalpha << (x0 == 0)
-                << "\n* x1==0.0 " << std::boolalpha << (x1 == 0.0)
-                << "\n* x2==0 " << std::boolalpha << (x2 == 0)
-                << "\n* x3==0.0f " << std::boolalpha << (x3 == 0.0f)
-                << "\n* x4==0 " << std::boolalpha << (x4 == 0)
-                << "\n* x5==0.0f " << std::boolalpha << (x5 == 0.0f)
-                << "\n* x6==0 " << std::boolalpha << (x6 == 0)
-                << "\n* x7==<null> " << std::boolalpha << is_null(x7)
-				<< std::endl;
+		std::cout << "---------- Test is_null() ----------" << std::endl;
 
-            std::cout << "Is null ? " << std::boolalpha << is_null(null) << " " << is_null(1) << std::endl;
-		}
+		test_condition("a is non null: ", !cpp_db::is_null(a) && !a.is_null());
+		test_condition("d is null: ", cpp_db::is_null(d) && d.is_null());
+		test_condition("e is non null: ", !cpp_db::is_null(e) && !e.is_null());
+		test_condition("f is null: ", cpp_db::is_null(f) && f.is_null());
+		test_condition("null is null: ", cpp_db::is_null(cpp_db::nullable_int()) && cpp_db::nullable_int().is_null());
 
-        std::unique_ptr<cpp_db::connection_option> p(cpp_db::make_key_value_pair(std::string("driver"), std::string("QIBASE")));
+		cpp_db::null_type null;
 
-        std::cout << p->key<std::string, std::string>()
-                  << "=\"" << p->value<std::string, std::string>() << "\""
-                  << std::endl;
+		TEST_VERIFY(cpp_db::is_null(null));
+		TEST_VERIFY(!cpp_db::is_null(1));
+
+		std::cout << "---------- Test coalesce ----------" << std::endl;
+
+        cpp_db::coalesce_trait<int, double, float>::type x0(0);
+        cpp_db::coalesce_trait<cpp_db::null_type, double, float>::type x1(0.0);
+        cpp_db::coalesce_trait<int, cpp_db::null_type, float>::type x2(0);
+        cpp_db::coalesce_trait<cpp_db::null_type, cpp_db::null_type, float>::type x3(0.0f);
+        cpp_db::coalesce_trait<int, double, cpp_db::null_type>::type x4(0);
+        cpp_db::coalesce_trait<cpp_db::null_type, double, cpp_db::null_type>::type x5(0.0f);
+        cpp_db::coalesce_trait<int, cpp_db::null_type, cpp_db::null_type>::type x6(0);
+		cpp_db::coalesce_trait<cpp_db::null_type, cpp_db::null_type, cpp_db::null_type>::type x7;
+
+		TEST_EQUAL(cpp_db::coalesce(1, 2, 3, 4), 1);
+		TEST_EQUAL(cpp_db::coalesce(cpp_db::null_type(), 2, 3, 4), 2);
+		TEST_VERIFY(cpp_db::is_null(cpp_db::coalesce(null, null, null)));
+		TEST_EQUAL(cpp_db::coalesce(1, null, null, null), 1);
+		TEST_EQUAL(cpp_db::coalesce(null, 2, null, 4), 2);
+            
+		TEST_EQUAL(x0, 0);
+		TEST_EQUAL(x1, 0.0);
+		TEST_EQUAL(x2, 0);
+		TEST_EQUAL(x3, 0.0f);
+		TEST_EQUAL(x4, 0);
+		TEST_EQUAL(x5, 0.0f);
+		TEST_EQUAL(x6, 0);
+		TEST_VERIFY(is_null(x7));
+
+		std::unique_ptr<cpp_db::connection_option> p(cpp_db::make_key_value_pair(std::string("driver"), std::string("QIBASE")));
+		TEST_EQUAL( (p->key<std::string, std::string>()), "driver");
+		TEST_EQUAL( (p->value<std::string, std::string>()) , std::string("QIBASE"));
+
+		std::cout << "========== Test ends ==========" << std::endl;
     }
     catch(const std::exception &ex)
     {
