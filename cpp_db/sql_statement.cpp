319cbc52a949b7fd54b9f2161c9b8c8a78645c01
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-02 13:51:08 +0200

    Add a constructor without sql command to sql_statement. Add prepare for sql_statement Add a is_prepared() function adjust names of executeXXX functions.

--- cpp_db/sql_statement.cpp
+++ cpp_db/sql_statement.cpp
@@ -20,40 +20,75 @@ struct sql_statement::impl
     sqlite3_stmt *stmt;
     sqlite3 *db;
     const char *tail;
+
+    void finalize()
+    {
+        if (stmt != nullptr)
+        {
+            sqlite3_finalize(stmt);
+            stmt = nullptr;
+        }
+    }
 };
 
-sql_statement::sql_statement(const std::string &sqlcmd, connection &conn)
+sql_statement::sql_statement(connection &conn)
     : pimpl(new impl)
 {
     pimpl->db = static_cast<sqlite3*>(conn.get_handle());
-    int error_code = sqlite3_prepare_v2(pimpl->db, sqlcmd.c_str(), sqlcmd.size(), &pimpl->stmt, &pimpl->tail);
-    if (error_code != SQLITE_OK)
-        throw_db_exception(error_code, pimpl->db);
+    if (!pimpl->db)
+        throw db_exception("No database connection for statement!");
+}
+
+sql_statement::sql_statement(const std::string &sqlcmd, connection &conn)
+    : sql_statement(conn)
+{
+    prepare(sqlcmd);
 }
 
 sql_statement::~sql_statement()
 {
-    if (pimpl->stmt != nullptr)
-        sqlite3_finalize(pimpl->stmt);
+    pimpl->finalize();
 }
 
-void sql_statement::executeDDL()
+void sql_statement::prepare(const std::string &sqlcmd)
+{
+    sqlite3_stmt *stmt = nullptr;
+    const char *tail = nullptr;
+
+    int error_code = sqlite3_prepare_v2(pimpl->db, sqlcmd.c_str(), sqlcmd.size(), &stmt, &tail);
+    if (error_code != SQLITE_OK)
+        throw_db_exception(error_code, pimpl->db);
+
+    pimpl->finalize();
+    pimpl->stmt = stmt;
+    pimpl->tail = tail;
+}
+
+void sql_statement::execute_ddl()
 {
 	execute();
 }
 
-void sql_statement::executeNonQuery()
+void sql_statement::execute_non_query()
 {
 	execute();
 }
 
 void sql_statement::execute()
 {
+    if (!is_prepared())
+        throw db_exception("Statement not prepared!");
+
 	if (int error_code = sqlite3_step(pimpl->stmt))
 	{
 		if (error_code != SQLITE_DONE && error_code != SQLITE_ROW)
 			throw_db_exception(error_code, pimpl->db);
 	}
+}
+
+bool sql_statement::is_prepared() const
+{
+    return pimpl->stmt != nullptr;
 }
 
 }
