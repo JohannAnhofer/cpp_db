c11a33c60033919722d025fa5b692e5715012a84
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-02 13:51:08 +0200

    Add a constructor without sql command to sql_statement. Add prepare for sql_statement Add a is_prepared() function adjust names of executeXXX functions.

--- cpp_db/sql_statement.h
+++ cpp_db/sql_statement.h
@@ -13,14 +13,19 @@ class sql_statement
 {
 public:
     sql_statement(const std::string &sqlcmd, connection &conn);
+    explicit sql_statement(connection &conn);
     ~sql_statement();
 
 	sql_statement(const sql_statement&other) = delete;
 	sql_statement &operator=(const sql_statement &other) = delete;
 
-    void executeDDL();
-	void executeNonQuery();
+    void prepare(const std::string &sqlcmd);
+
+    void execute_ddl();
+    void execute_non_query();
 	void execute();
+
+    bool is_prepared() const;
 
 private:
     struct impl;
--- cpp_db/test_cpp_db.cpp
+++ cpp_db/test_cpp_db.cpp
@@ -1,4 +1,4 @@
-#include "test.h"
+﻿#include "test.h"
 
 #include "db_exception.h"
 #include "connection.h"
@@ -12,11 +12,14 @@ void test_cpp_db()
 	{
 		cpp_db::connection con("sqlite");
 		TEST_FOR_NO_EXCPTION(con.open(":memory:"));
-		TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("create table TEST_TABLE(COL1 INT, COL2 VARCHAR(50));", con).executeDDL());
-		TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(1, 'first')", con).executeNonQuery());
-		TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(2, 'second')", con).executeNonQuery());
-		TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(3, 'third')", con).executeNonQuery());
+        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("create table TEST_TABLE(COL1 INT, COL2 VARCHAR(50));", con).execute_ddl());
+        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(1, 'first')", con).execute_non_query());
+        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(2, 'second')", con).execute_non_query());
+        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(3, 'third')", con).execute_non_query());
 		TEST_FOR_EXCEPTION(cpp_db::sql_statement("select COL1, COL2, COL3 from TEST_TABLE;", con).execute(), cpp_db::db_exception);
+        cpp_db::sql_statement stmt(con);
+        TEST_FOR_NO_EXCPTION(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(?, ?)"));
+        TEST_FOR_EXCEPTION(stmt.execute_non_query(), cpp_db::db_exception);
 		TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("select COL1, COL2 from TEST_TABLE;", con).execute());
 	}
 	catch (const cpp_db::db_exception &e)
