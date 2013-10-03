75958d04db1dffc36ce73ad97762242eba5b75eb
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-03 11:31:24 +0200

    Fix testcase when executing a statement without values for parameters supplied.

--- cpp_db/test_cpp_db.cpp
+++ cpp_db/test_cpp_db.cpp
@@ -12,7 +12,7 @@ void test_cpp_db()
 	{
 		cpp_db::connection con("sqlite");
 		TEST_FOR_NO_EXCPTION(con.open(":memory:"));
-        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("create table TEST_TABLE(COL1 INT, COL2 VARCHAR(50));", con).execute_ddl());
+        TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("create table TEST_TABLE(COL1 INT NOT NULL, COL2 VARCHAR(50));", con).execute_ddl());
         TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(1, 'first')", con).execute_non_query());
         TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(2, 'second')", con).execute_non_query());
         TEST_FOR_NO_EXCPTION(cpp_db::sql_statement("insert into TEST_TABLE(COL1, COL2) VALUES(3, 'third')", con).execute_non_query());
