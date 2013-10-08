2bf4d31bedf34a0e27a8b4072ee0a184458b67c3
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-08 11:31:21 +0200

    Move parameter functions from statement.cpp to parameters.cpp. Add parameters  and parameter class. Add testcases for parameter class.

---
+++ cpp_db/parameter.h
@@ -0,0 +1,116 @@
+#ifndef CPP_DB_PARAMETER_H
+#define CPP_DB_PARAMETER_H
+
+#include "db_exception.h"
+
+#include <string>
+#include <memory>
+
+namespace cpp_db
+{
+
+	class parameter
+	{
+	public:
+		template<typename T>
+		parameter(int pos, T value)
+			: pholder(new concrete_holder<int, T>(pos, value))
+		{
+		}
+
+		template<typename T>
+		parameter(std::string name, T value)
+			: pholder(new concrete_holder<std::string, T>(name, value))
+		{
+		}
+
+		template<typename T>
+		T get_value() const
+		{
+			if (pholder->get_value_type() == typeid(T))
+				return *reinterpret_cast<T const *>(pholder->get_value());
+			throw db_exception("Invalid value type for parameter.");
+		}
+
+		bool has_index() const
+		{
+			return pholder->get_index_type() == typeid(int);
+		}
+
+		template<typename T>
+		bool has_value_of_type() const
+		{
+			return pholder->get_value_type() == typeid(T);
+		}
+
+		bool has_name() const
+		{
+			return pholder->get_index_type() == typeid(std::string);
+		}
+
+		int get_index() const
+		{
+			if (pholder->get_index_type() == typeid(int))
+				return *reinterpret_cast<int const *>(pholder->get_index());
+			throw db_exception("Invalid index type for parameter.");
+		}
+
+		std::string get_name() const
+		{
+			if (pholder->get_index_type() == typeid(std::string))
+				return *reinterpret_cast<std::string const *>(pholder->get_index());
+			throw db_exception("Invalid index type for parameter.");
+		}
+
+	private:
+		struct abstract_holder
+		{
+			virtual ~abstract_holder() {}
+			virtual void const * get_value() const = 0;
+			virtual const type_info &get_value_type() const = 0;
+			virtual void const * get_index() const = 0;
+			virtual const type_info &get_index_type() const = 0;
+		};
+
+		template<typename IndexType, typename ValueType>
+		struct concrete_holder : public abstract_holder
+		{
+			explicit concrete_holder(IndexType index_in, ValueType value_in)
+				: index(index_in)
+				, value(value_in)
+				, index_type(typeid(index_in))
+				, value_type(typeid(value_in))
+			{
+			}
+
+			void const * get_value() const override
+			{
+				return &value;
+			}
+
+			const type_info &get_value_type() const override
+			{
+				return value_type;
+			}
+
+			void const * get_index() const override
+			{
+				return &index;
+			}
+
+			const type_info & get_index_type() const override
+			{
+				return index_type;
+			}
+
+			IndexType index;
+			ValueType value;
+			const type_info &index_type, &value_type;
+		};
+
+		std::unique_ptr<abstract_holder> pholder;
+	};
+
+}
+
+#endif
--- main/test_cpp_db.cpp
+++ main/test_cpp_db.cpp
@@ -4,6 +4,9 @@
 #include "connection.h"
 #include "statement.h"
 #include "record.h"
+#include "parameters.h"
+
+#include <cmath>
 
 void test_cpp_db()
 {
@@ -43,6 +46,22 @@ void test_cpp_db()
 		TEST_EQUAL(result.get_field_value(1), "first");
 
         TEST_FOR_EXCEPTION(cpp_db::record(cpp_db::statement(con)), cpp_db::db_exception);
+
+		cpp_db::parameter param1(0, std::string("Hello world!")), param2(std::string("@test"), 27.85);
+
+		TEST_VERIFY(param1.has_index());
+		TEST_VERIFY(!param1.has_name());
+		TEST_VERIFY(!param2.has_index());
+		TEST_VERIFY(param2.has_name());
+		TEST_VERIFY(param1.has_value_of_type<std::string>());
+		TEST_VERIFY(!param1.has_value_of_type<double>());
+		TEST_VERIFY(!param2.has_value_of_type<std::string>());
+		TEST_VERIFY(param2.has_value_of_type<double>());
+
+		TEST_EQUAL(param1.get_value<std::string>(), "Hello world!");
+		TEST_VERIFY(std::fabs(param2.get_value<double>() - 27.85) < 0.000001);
+
+		TEST_FOR_EXCEPTION(param1.get_value<int>(), cpp_db::db_exception);
 	}
 	catch (const cpp_db::db_exception &e)
 	{
