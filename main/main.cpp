6cc21bfbfe91bd50a91bbb393c2f54c9684d4946
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-09 11:44:06 +0200

    Add header files for test classes.

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include "test_tools_class.h"
+#include "test_cpp_db_class.h"
 
-void test_cpp_db();
-void test_tools();
+#include <iostream>
 
 int main(int , char *[])
 {
     std::cout << std::endl;
 
-	test_tools();
-	test_cpp_db();
+    test_tools_class()();
+    test_cpp_db_class()();
 
 	return 0;
 }
---
+++ main/test_tools_class.h
@@ -0,0 +1,25 @@
+#ifndef TEST_TOOLS_CLASS_H
+#define TEST_TOOLS_CLASS_H
+
+#include "abstract_test.h"
+
+class test_tools_class : public test::abstract_test
+{
+public:
+    BEGIN_DECLARE_TEST_CLASS(test_tools_class)
+    DECLARE_TEST_FUNCTION(test_test_functions)
+    DECLARE_TEST_FUNCTION(test_nullable)
+    DECLARE_TEST_FUNCTION(test_is_null)
+    DECLARE_TEST_FUNCTION(test_coalesce)
+    DECLARE_TEST_FUNCTION(test_key_value_pair)
+    END_DECLARE_TEST_CLASS()
+
+protected:
+    void test_test_functions();
+    void test_nullable();
+    void test_is_null();
+    void test_coalesce();
+    void test_key_value_pair();
+};
+
+#endif // TEST_TOOLS_CLASS_H
