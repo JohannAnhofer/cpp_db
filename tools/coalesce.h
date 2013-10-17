67f8d3a9d95f140a48067686efa58194933923b9
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-17 09:25:12 +0200

    Move coalesce.h back to tools. Add a tools::null type and a tools::is_null() function. Move related test cases also.

--- tools/coalesce.h
+++ tools/coalesce.h
@@ -2,10 +2,11 @@
 #define CPP_DB_COALESCE_H
 
 #include "cpp11_defines.h"
-#include "null.h"
 
-namespace cpp_db
+namespace tools
 {
+
+struct null_type {};
 
 // first we need a trait structure to deduce the return type of coalesce
 
@@ -64,6 +65,17 @@ typename coalesce_trait<Ts...>::type coalesce(null_type, Ts... ts)
 CONSTEXPR null_type coalesce(null_type nt)
 {
 	return nt;
+}
+
+inline CONSTEXPR bool is_null(null_type)
+{
+    return true;
+}
+
+template<typename T>
+CONSTEXPR bool is_null(const T &)
+{
+    return false;
 }
 
 }
