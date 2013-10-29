c7949580f9dc1015f020afc996fca435e24e007d
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-29 16:04:54 +0100

    Implement junit xml output.

--- test/junit_output.h
+++ test/junit_output.h
@@ -5,6 +5,9 @@
 
 #include <iosfwd>
 #include <string>
+#include <vector>
+#include <map>
+#include <chrono>
 
 namespace test
 {
@@ -26,8 +29,49 @@ public:
     void output_exception(const std::string &message, int line, const char * file) override;
     void output_message(const std::string &message, int line, const char * file) override;
 
+
 private:
-    std::ostream *output;
+	// types
+	using timepoint = std::chrono::time_point<std::chrono::system_clock>;
+
+	struct test_case
+	{
+		std::string message;
+		std::string failure_type;
+		timepoint start;
+		timepoint end;
+		int line;
+		const char *file;
+	};
+
+	struct test_function
+	{
+		std::string name;
+		std::string class_name;
+		timepoint start;
+		timepoint end;
+		std::vector<test_case> test_cases;
+	};
+	
+	struct test_suite
+	{
+		std::string name;
+		std::string timestamp;
+		std::string hostname;
+		int tests, failures, errors;
+		timepoint start;
+		timepoint end;
+		std::map<std::string, test_function> test_functions;
+	};
+
+	// functions
+	void add_test_case(const std::string &type, const std::string &message, int line, const char * file);
+	void write_xml() const;
+
+	// data
+	std::ostream *output;
+	std::map<std::string, test_suite> test_suites;
+	test_function *current_test_function;	
 };
 
 } // namespace test
