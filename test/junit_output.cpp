61b691b68059723c2d26efe9877a82c01e305a4f
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-29 16:04:54 +0100

    Implement junit xml output.

--- test/junit_output.cpp
+++ test/junit_output.cpp
@@ -3,12 +3,28 @@
 
 #include <stdexcept>
 #include <ostream>
+#include <ctime>
+#include <vector>
+
+// Get current date/time, format is ISO8601
+std::string get_current_timestamp_iso8601()
+{
+    time_t now = time(0);
+    const tm  tstruct = *localtime(&now);
+    char buf[80];
+
+    if (strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &tstruct))
+        return buf;
+    else
+        return std::string();
+}
 
 namespace test
 {
 
 junit_output::junit_output(std::ostream *output_stream)
     : output(output_stream)
+	, current_test_function(nullptr)
 {
     if (!output)
         throw std::runtime_error("No outputstream");
@@ -16,57 +32,94 @@ junit_output::junit_output(std::ostream *output_stream)
 
 void junit_output::start()
 {
+	test_suites.clear();
 }
 
 void junit_output::start_class(const std::string &class_name)
 {
-    (void)class_name;
+	test_suite suite;
+	suite.name = class_name;
+	suite.timestamp = get_current_timestamp_iso8601();
+	suite.hostname = "localhost";
+	suite.tests = suite.failures = suite.errors = 0;
+	suite.end = suite.start = std::chrono::system_clock::now();
+	test_suites.insert(std::make_pair(class_name, suite));
 }
 
 void junit_output::start_function(const std::string &class_name, const std::string &function_name)
 {
-    (void)class_name;
-    (void)function_name;
+	test_function function;
+	function.name = function_name;
+	function.class_name = class_name;
+	function.end = function.start = std::chrono::system_clock::now();
+	test_suites[class_name].test_functions.insert(make_pair(function_name, function));
+	current_test_function = &test_suites[class_name].test_functions[function_name];
 }
 
 void junit_output::end_function(const std::string &class_name, const std::string &function_name)
 {
-    (void)class_name;
-    (void)function_name;
+	test_suites[class_name].test_functions[function_name].end = std::chrono::system_clock::now();
+	current_test_function = nullptr;
 }
 
 void junit_output::end_class(const std::string &class_name, const test_class_statistics &stats)
 {
-    (void)class_name;
-    (void)stats;
+	auto &test_suite = test_suites[class_name];
+
+	test_suite.end = std::chrono::system_clock::now();
+	test_suite.tests = stats.success_count + stats.fail_count;
+	test_suite.failures = stats.fail_count - (stats.exception_count - stats.expected_exception_count);
+	test_suite.errors = stats.exception_count - stats.expected_exception_count;
 }
 
 void junit_output::end(int class_count, const test_class_statistics &stats)
 {
+	write_xml();
+	test_suites.clear();
+	current_test_function = nullptr;
+
     (void)class_count;
     (void)stats;
 }
 
+void junit_output::add_test_case(const std::string &type, const std::string &message, int line, const char * file)
+{
+	if (current_test_function)
+	{
+		test_case tcase;
+
+		tcase.message = message;
+		tcase.failure_type = type;
+		if (current_test_function->test_cases.empty())
+			tcase.start = current_test_function->start;
+		else
+			tcase.start = current_test_function->test_cases.back().end;
+		tcase.end = std::chrono::system_clock::now();
+		tcase.line = line;
+		tcase.file = file;
+		current_test_function->test_cases.push_back(tcase);
+	}
+}
 
 void junit_output::output_success(const std::string &message, int line, const char *file)
 {
-    (void)line;
+	add_test_case(std::string(), message, line, file);
+	(void)line;
     (void)file;
-    (void)message;
 }
 
 void junit_output::output_failure(const std::string &message, int line, const char *file)
 {
+	add_test_case("failure", message, line, file);
     (void)line;
     (void)file;
-    (void)message;
 }
 
 void junit_output::output_exception(const std::string &message, int line, const char *file)
 {
+	add_test_case("error", message, line, file);
     (void)line;
     (void)file;
-    (void)message;
 }
 
 void junit_output::output_message(const std::string &message, int line, const char *file)
@@ -74,6 +127,72 @@ void junit_output::output_message(const std::string &message, int line, const ch
     (void)line;
     (void)file;
     (void)message;
+}
+
+using timepoint = std::chrono::time_point<std::chrono::system_clock>;
+
+inline double duration_in_seconds(const timepoint &start, const timepoint &end)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0;
+}
+
+void junit_output::write_xml() const
+{
+	*output << "<?xml version = '1.0' encoding = 'ASCII'?>\n"
+		<< "<testsuites"
+		<< " xmlns:xsi='" << "http://www.w3.org/2001/XMLSchema-instance" << "'"
+		<< " xsi:noNamespaceSchemaLocation='" << "http://windyroad.org/dl/Open%20Source/JUnit.xsd" << "'"
+		<< ">\n";
+
+	int id{ 0 };
+
+	for (const auto &suite_entry : test_suites)
+	{
+		const test_suite &suite = suite_entry.second;
+
+		*output << "\t<testsuite"
+			<< " id='" << id++ << "'"
+			<< " package='" << suite_entry.first << "'"
+			<< " name='" << suite.name << "'"
+			<< " timestamp='" << suite.timestamp << "'"
+			<< " hostname='" << "localhost" << "'"
+			<< " tests='" << suite.tests << "'"
+			<< " failures='" << suite.failures << "'"
+			<< " errors='" << suite.errors << "'"
+			<< " time='" << duration_in_seconds(suite.start, suite.end) << "'"
+			<< ">\n"
+			<< "\t\t<properties />\n";
+
+		int tc_count{ 0 };
+
+		for (const auto &tf : suite.test_functions)
+		{
+			const auto &test_function = tf.second;
+
+			for (const auto &test_case : test_function.test_cases)
+			{
+				*output << "\t\t<testcase"
+					<< " name='test_case_" << tc_count++ << "_line_" << test_case.line << "'"
+					<< " classname='" << test_function.class_name << "::" << test_function.name << "'"
+					<< " time='" << duration_in_seconds(test_case.start, test_case.end) << "'";
+
+				if (test_case.failure_type.empty())
+					*output << " />\n";
+				else
+					*output << " >\n"
+							<< "\t\t\t<" << test_case.failure_type 
+							<< " type='" << test_case.failure_type << "'"
+							<< " message='" << test_case.message << "'"
+							<< " />\n"
+							<< "\t\t</testcase>\n";
+			}
+		}
+
+		*output << "\t\t<system-out />\n"
+			<< "\t\t<system-err />\n"
+			<< "\t</testsuite>\n";
+	}
+	*output << "</testsuites>\n";
 }
 
 } // namespace test
