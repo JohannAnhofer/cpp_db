9d4b3d8f2349f47fc39944eb3c7820a0c077a771
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-10-29 17:38:38 +0100

    Apply pimpl idiom.

--- test/junit_output.cpp
+++ test/junit_output.cpp
@@ -6,137 +6,80 @@
 #include <ctime>
 #include <vector>
 
-// Get current date/time, format is ISO8601
-std::string get_current_timestamp_iso8601()
-{
-    time_t now = time(0);
-    const tm  tstruct = *localtime(&now);
-    char buf[80];
-
-    if (strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &tstruct))
-        return buf;
-    else
-        return std::string();
-}
-
 namespace test
 {
 
-junit_output::junit_output(std::ostream *output_stream)
-    : output(output_stream)
-	, current_test_function(nullptr)
-{
-    if (!output)
-        throw std::runtime_error("No outputstream");
-}
-
-void junit_output::start()
-{
-	test_suites.clear();
-}
-
-void junit_output::start_class(const std::string &class_name)
-{
-	test_suite suite;
-	suite.name = class_name;
-	suite.timestamp = get_current_timestamp_iso8601();
-	suite.hostname = "localhost";
-	suite.tests = suite.failures = suite.errors = 0;
-	suite.end = suite.start = std::chrono::system_clock::now();
-	test_suites.insert(std::make_pair(class_name, suite));
-}
-
-void junit_output::start_function(const std::string &class_name, const std::string &function_name)
-{
-	test_function function;
-	function.name = function_name;
-	function.class_name = class_name;
-	function.end = function.start = std::chrono::system_clock::now();
-	test_suites[class_name].test_functions.insert(make_pair(function_name, function));
-	current_test_function = &test_suites[class_name].test_functions[function_name];
-}
-
-void junit_output::end_function(const std::string &class_name, const std::string &function_name)
-{
-	test_suites[class_name].test_functions[function_name].end = std::chrono::system_clock::now();
-	current_test_function = nullptr;
-}
-
-void junit_output::end_class(const std::string &class_name, const test_class_statistics &stats)
-{
-	auto &test_suite = test_suites[class_name];
-
-	test_suite.end = std::chrono::system_clock::now();
-	test_suite.tests = stats.success_count + stats.fail_count;
-	test_suite.failures = stats.fail_count - (stats.exception_count - stats.expected_exception_count);
-	test_suite.errors = stats.exception_count - stats.expected_exception_count;
-}
-
-void junit_output::end(int class_count, const test_class_statistics &stats)
-{
-	write_xml();
-	test_suites.clear();
-	current_test_function = nullptr;
-
-    (void)class_count;
-    (void)stats;
-}
-
-void junit_output::add_test_case(const std::string &type, const std::string &message, int line, const char * file)
-{
-	if (current_test_function)
-	{
-		test_case tcase;
-
-		tcase.message = message;
-		tcase.failure_type = type;
-		if (current_test_function->test_cases.empty())
-			tcase.start = current_test_function->start;
-		else
-			tcase.start = current_test_function->test_cases.back().end;
-		tcase.end = std::chrono::system_clock::now();
-		tcase.line = line;
-		tcase.file = file;
-		current_test_function->test_cases.push_back(tcase);
-	}
-}
-
-void junit_output::output_success(const std::string &message, int line, const char *file)
-{
-	add_test_case(std::string(), message, line, file);
-	(void)line;
-    (void)file;
-}
-
-void junit_output::output_failure(const std::string &message, int line, const char *file)
-{
-	add_test_case("failure", message, line, file);
-    (void)line;
-    (void)file;
-}
-
-void junit_output::output_exception(const std::string &message, int line, const char *file)
-{
-	add_test_case("error", message, line, file);
-    (void)line;
-    (void)file;
-}
-
-void junit_output::output_message(const std::string &message, int line, const char *file)
-{
-    (void)line;
-    (void)file;
-    (void)message;
-}
-
 using timepoint = std::chrono::time_point<std::chrono::system_clock>;
 
-inline double duration_in_seconds(const timepoint &start, const timepoint &end)
+// Get current date/time, format is ISO8601
+static std::string get_current_timestamp_iso8601()
+{
+	time_t now = time(0);
+	const tm  tstruct = *localtime(&now);
+	char buf[80];
+
+	if (strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &tstruct))
+		return buf;
+	else
+		return std::string();
+}
+
+static inline double duration_in_seconds(const timepoint &start, const timepoint &end) 
 {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0;
 }
 
-void junit_output::write_xml() const
+struct test_case
+{
+	std::string message;
+	std::string failure_type;
+	timepoint start;
+	timepoint end;
+	int line;
+	const char *file;
+};
+
+struct test_function
+{
+	std::string name;
+	std::string class_name;
+	timepoint start;
+	timepoint end;
+	std::vector<test_case> test_cases;
+};
+
+struct test_suite
+{
+	std::string name;
+	std::string timestamp;
+	std::string hostname;
+	int tests, failures, errors;
+	timepoint start;
+	timepoint end;
+	std::map<std::string, test_function> test_functions;
+};
+
+struct junit_output::impl
+{
+	explicit impl(std::ostream *output_stream);
+
+	// functions
+	void add_test_case(const std::string &type, const std::string &message, int line, const char * file);
+	void write_xml() const;
+
+	// data
+	std::ostream *output;
+	std::map<std::string, test_suite> test_suites;
+	test_function *current_test_function;
+};
+
+junit_output::impl::impl(std::ostream *output_stream)
+: output{ output_stream }
+, current_test_function{ nullptr }
+{
+}
+
+void junit_output::impl::write_xml() const
 {
 	*output << "<?xml version = '1.0' encoding = 'ASCII'?>\n"
 		<< "<testsuites"
@@ -180,11 +123,11 @@ void junit_output::write_xml() const
 					*output << " />\n";
 				else
 					*output << " >\n"
-							<< "\t\t\t<" << test_case.failure_type 
-							<< " type='" << test_case.failure_type << "'"
-							<< " message='" << test_case.message << "'"
-							<< " />\n"
-							<< "\t\t</testcase>\n";
+					<< "\t\t\t<" << test_case.failure_type
+					<< " type='" << test_case.failure_type << "'"
+					<< " message='" << test_case.message << "'"
+					<< " />\n"
+					<< "\t\t</testcase>\n";
 			}
 		}
 
@@ -193,6 +136,118 @@ void junit_output::write_xml() const
 			<< "\t</testsuite>\n";
 	}
 	*output << "</testsuites>\n";
+}
+
+void junit_output::impl::add_test_case(const std::string &type, const std::string &message, int line, const char * file)
+{
+	if (current_test_function)
+	{
+		test_case tcase;
+
+		tcase.message = message;
+		tcase.failure_type = type;
+		if (current_test_function->test_cases.empty())
+			tcase.start = current_test_function->start;
+		else
+			tcase.start = current_test_function->test_cases.back().end;
+		tcase.end = std::chrono::system_clock::now();
+		tcase.line = line;
+		tcase.file = file;
+		current_test_function->test_cases.push_back(tcase);
+	}
+}
+
+// junit_output implemenation 
+
+junit_output::junit_output(std::ostream *output_stream)
+	: pimpl(new impl(output_stream))
+{
+    if (!pimpl->output)
+        throw std::runtime_error("No outputstream");
+}
+
+junit_output::~junit_output()
+{
+}
+
+void junit_output::start()
+{
+	pimpl->test_suites.clear();
+}
+
+void junit_output::start_class(const std::string &class_name)
+{
+	test_suite suite;
+	suite.name = class_name;
+	suite.timestamp = get_current_timestamp_iso8601();
+	suite.hostname = "localhost";
+	suite.tests = suite.failures = suite.errors = 0;
+	suite.end = suite.start = std::chrono::system_clock::now();
+	pimpl->test_suites.insert(std::make_pair(class_name, suite));
+}
+
+void junit_output::start_function(const std::string &class_name, const std::string &function_name)
+{
+	test_function function;
+	function.name = function_name;
+	function.class_name = class_name;
+	function.end = function.start = std::chrono::system_clock::now();
+	pimpl->test_suites[class_name].test_functions.insert(make_pair(function_name, function));
+	pimpl->current_test_function = &pimpl->test_suites[class_name].test_functions[function_name];
+}
+
+void junit_output::end_function(const std::string &class_name, const std::string &function_name)
+{
+	pimpl->test_suites[class_name].test_functions[function_name].end = std::chrono::system_clock::now();
+	pimpl->current_test_function = nullptr;
+}
+
+void junit_output::end_class(const std::string &class_name, const test_class_statistics &stats)
+{
+	auto &test_suite = pimpl->test_suites[class_name];
+
+	test_suite.end = std::chrono::system_clock::now();
+	test_suite.tests = stats.success_count + stats.fail_count;
+	test_suite.failures = stats.fail_count - (stats.exception_count - stats.expected_exception_count);
+	test_suite.errors = stats.exception_count - stats.expected_exception_count;
+}
+
+void junit_output::end(int class_count, const test_class_statistics &stats)
+{
+	pimpl->write_xml();
+	pimpl->test_suites.clear();
+	pimpl->current_test_function = nullptr;
+
+    (void)class_count;
+    (void)stats;
+}
+
+void junit_output::output_success(const std::string &message, int line, const char *file)
+{
+	pimpl->add_test_case(std::string(), message, line, file);
+	(void)line;
+    (void)file;
+}
+
+void junit_output::output_failure(const std::string &message, int line, const char *file)
+{
+	pimpl->add_test_case("failure", message, line, file);
+    (void)line;
+    (void)file;
+}
+
+void junit_output::output_exception(const std::string &message, int line, const char *file)
+{
+	pimpl->add_test_case("error", message, line, file);
+    (void)line;
+    (void)file;
+}
+
+void junit_output::output_message(const std::string &message, int line, const char *file)
+{
+    (void)line;
+    (void)file;
+    (void)message;
 }
 
 } // namespace test
