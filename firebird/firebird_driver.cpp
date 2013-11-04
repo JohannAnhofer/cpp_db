7d01f2ff0d7b25a1f1a879c18af3c26cde636f4e
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-11-04 14:02:00 +0100

    Add firebird_statement.h/.cpp and firebird_transaction.h/.cpp to VS project. Let the firebird driver return objects of firebird_statement and firebird_transaction. Fix handling of firebird handles.

--- firebird/firebird_driver.cpp
+++ firebird/firebird_driver.cpp
@@ -1,10 +1,10 @@
 #include "firebird_driver.h"
 #include "firebird_connection.h"
-/*
+#include "firebird_transaction.h"
 #include "firebird_statement.h"
+/*
 #include "firebird_parameters.h"
 #include "firebird_result.h"
-#include "firebird_transaction.h"
 */
 
 namespace cpp_db
@@ -26,9 +26,7 @@ namespace cpp_db
 
     statement_interface *firebird_driver::make_statement(const handle &conn_handle) const
     {
-        (void)conn_handle;
-        return nullptr;
-//        return new firebird_statement(conn_handle);
+        return new firebird_statement(conn_handle);
     }
 
     parameters_interface *firebird_driver::make_parameters(const handle &stmt_handle) const
@@ -40,16 +38,14 @@ namespace cpp_db
 
     result_interface *firebird_driver::make_result(const handle &stmt_handle) const
     {
-        (void)stmt_handle;
-        return nullptr;
-//        return new firebird_result(stmt_handle);
+		(void)(void)stmt_handle;
+		return nullptr;
+//		return new firebird_result(stmt_handle);
     }
 
     transaction_interface *firebird_driver::make_transaction(const handle &conn_handle) const
     {
-        (void)conn_handle;
-        return nullptr;
-//        return new firebird_transaction(conn_handle);
+		return new firebird_transaction(conn_handle);
     }
 
 }
