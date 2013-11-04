464bed76862c296f95a2c4def6e1ac00b8cca729
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-11-04 11:48:56 +0100

    Fix handle for firebird connection

--- main/test_firebird_class.cpp
+++ main/test_firebird_class.cpp
@@ -5,7 +5,10 @@
 void test_firebird_class::init_class()
 {
 	con.reset(new cpp_db::connection(std::make_shared<cpp_db::firebird_driver>()));
-	TEST_FOR_NO_EXCEPTION(con->open("arthur.roche.com:/home2/anhoferj/be3_trunk/data/Database/be3db.fdb", cpp_db::user_password_authentication{"SYSDBA", "@mo1di2@" }));
+	TEST_FOR_NO_EXCEPTION(con->open("arthur.roche.com:/home2/anhoferj/be3_trunk/data/Database/be3db.fdb", 
+							cpp_db::user_password_authentication{ "SYSDBA", "@mo1di2@" },
+							{ { "encoding", "UNICODE_FSS" }, {"role", "admin"} }
+						));
 }
 
 void test_firebird_class::cleanup_class()
