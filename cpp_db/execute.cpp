2e14cd895d4f269b200496708cf5e2c7eac7834b
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-11-14 16:12:45 +0100

    simplify execute... implementation

--- cpp_db/execute.cpp
+++ cpp_db/execute.cpp
@@ -5,37 +5,32 @@
 namespace cpp_db
 {
 
-template<typename Result>
-static Result execute_helper(const std::function<Result(statement &stmt)> &function, connection &conn, const std::string &sql, std::initializer_list<parameter> params)
-{
-    statement stmt(conn);
-    stmt.prepare(sql);
-    parameters p(stmt.get_parameters());
-
-    for (auto param: params)
-        p.bind(param);
-
-    return function(stmt);
-}
-
 void execute_non_query(connection &conn, const std::string &sql, std::initializer_list<parameter> params)
 {
-    execute_helper<void>([](statement &stmt){stmt.execute_non_query();}, conn, sql, params);
+	statement stmt(conn);
+	stmt.prepare(sql);
+	stmt.execute_non_query(params);
 }
 
 void execute_ddl(connection &conn, const std::string &sql, std::initializer_list<parameter> params)
 {
-    execute_helper<void>([](statement &stmt){stmt.execute_ddl();}, conn, sql, params);
+	statement stmt(conn);
+	stmt.prepare(sql);
+	stmt.execute_ddl(params);
 }
 
 value execute_scalar(connection &conn, const std::string &sql, std::initializer_list<parameter> params)
 {
-    return execute_helper<value>([](statement &stmt){return stmt.execute_scalar();}, conn, sql, params);
+	statement stmt(conn);
+	stmt.prepare(sql);
+	return stmt.execute_scalar(params);
 }
 
 result execute(connection &conn, const std::string &sql, std::initializer_list<parameter> params)
 {
-    return execute_helper<result>([](statement &stmt){return stmt.execute();}, conn, sql, params);
+	statement stmt(conn);
+	stmt.prepare(sql);
+	return stmt.execute(params);
 }
 
 }
