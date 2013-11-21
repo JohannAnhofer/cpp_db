bdf6ecdb1a93e84bf8e5ee583b37c5410df4e53d
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-11-21 14:30:42 +0100

    extend lock_or_throw with a exception message as context.

--- firebird/firebird_transaction.cpp
+++ firebird/firebird_transaction.cpp
@@ -34,7 +34,7 @@ handle firebird_transaction::get_handle() const
 
 isc_db_handle *firebird_transaction::get_db_handle() const
 {
-    return std::static_pointer_cast<isc_db_handle>(tools::lock_or_throw(conn_impl)->get_handle()).get();
+	return std::static_pointer_cast<isc_db_handle>(tools::lock_or_throw(conn_impl, "Invalid database connection")->get_handle()).get();
 }
 
 void firebird_transaction::begin()
