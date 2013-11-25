3fcb592ac460a5dc5152e7ed5ddb44bb9f25804e
DI Johann Anhofer <johann.anhofer@gmail.com>
2013-11-25 09:32:20 +0100

    Use namespace prefix cpp_db

--- main/test_cpp_db_class.cpp
+++ main/test_cpp_db_class.cpp
@@ -12,31 +12,31 @@ void test_cpp_db_class::test_is_null()
 {
 	cpp_db::null_type null;
 
-    TEST_VERIFY(is_null(null));
+    TEST_VERIFY(cpp_db::is_null(null));
 	TEST_VERIFY(!cpp_db::is_null(1));
 
 	cpp_db::value vnull(null);
-	TEST_VERIFY(is_null(vnull));
+    TEST_VERIFY(cpp_db::is_null(vnull));
 
 	cpp_db::parameter pnull(0, null);
-    TEST_VERIFY(is_null(pnull));
+    TEST_VERIFY(cpp_db::is_null(pnull));
 
-    TEST_FOR_EXCEPTION(type_of(null), std::logic_error);
-    TEST_FOR_EXCEPTION(type_of(vnull), std::logic_error);
-    TEST_FOR_EXCEPTION(type_of(pnull), std::logic_error);
+    TEST_FOR_EXCEPTION(cpp_db::type_of(null), std::logic_error);
+    TEST_FOR_EXCEPTION(cpp_db::type_of(vnull), std::logic_error);
+    TEST_FOR_EXCEPTION(cpp_db::type_of(pnull), std::logic_error);
 
-    TEST_FOR_EXCEPTION(value_of(null), std::logic_error);
+    TEST_FOR_EXCEPTION(cpp_db::value_of(null), std::logic_error);
     TEST_FOR_EXCEPTION(cpp_db::value_of<int>(vnull), std::logic_error);
     TEST_FOR_EXCEPTION(cpp_db::value_of<int>(pnull), std::logic_error);
 }
 
 void test_cpp_db_class::test_value()
 {
-	TEST_FOR_NO_EXCEPTION((cpp_db::value(10).cast_to<int, int64_t>()));
-	TEST_FOR_NO_EXCEPTION((cpp_db::value(10).cast_to<int, double>()));
-	TEST_FOR_NO_EXCEPTION((cpp_db::value(10.0).cast_to<double, int64_t>()));
-	TEST_FOR_EXCEPTION((cpp_db::value(10).cast_to<double, int64_t>()), std::runtime_error);
-    TEST_FOR_EXCEPTION((cpp_db::value(10.0).cast_to<long double, float>()), std::runtime_error);
+    TEST_FOR_NO_EXCEPTION((cpp_db::cast_to<int, int64_t>(cpp_db::value(10))));
+    TEST_FOR_NO_EXCEPTION((cpp_db::cast_to<int, double>(cpp_db::value(10))));
+    TEST_FOR_NO_EXCEPTION((cpp_db::cast_to<double, int64_t>(cpp_db::value(10.0))));
+    TEST_FOR_EXCEPTION((cpp_db::cast_to<double, int64_t>(cpp_db::value(10))), std::runtime_error);
+    TEST_FOR_EXCEPTION((cpp_db::cast_to<long double, float>(cpp_db::value(10.0))), std::runtime_error);
 
     TEST_EQUAL(cpp_db::type_of(10), typeid(int));
     TEST_EQUAL(cpp_db::type_of(cpp_db::value{10}), typeid(int));
