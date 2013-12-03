dd1fbfe92d3c45bf6632171e91a155e077c74dc1
Johann Anhofer <johann.anhofer@gmail.com>
2013-12-03 10:33:19 +0100

    Make VS Project compilable. Fix 1. Sample

--- samples/main.cpp
+++ samples/main.cpp
@@ -10,6 +10,9 @@ int main()
 {
     cpp_db::driver_factory::instance().register_driver("sqlite", []{return cpp_db::sqlite_driver::create();});
     cpp_db::driver_factory::instance().register_driver("firebird", []{return cpp_db::firebird_driver::create();});
+
+	run_samples();
+
     return 0;
 }
 
