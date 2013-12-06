4a5ee6bf01103b00c578e3121f1899770ca6aeae
Johann Anhofer <johann.anhofer@gmail.com>
2013-12-06 10:27:01 +0100

    Driver factory is not moveable nor copyable.

--- cpp_db/driver_factory.h
+++ cpp_db/driver_factory.h
@@ -15,6 +15,12 @@ namespace cpp_db
 class driver_factory
 {
 public:
+    driver_factory(const driver_factory &) = delete;
+    driver_factory &operator=(const driver_factory &) = delete;
+
+    driver_factory(driver_factory &&) = delete;
+    driver_factory &operator=(driver_factory &&) = delete;
+
     static driver_factory &instance();
 
     using driver_creator_function = std::function<driver_interface *()>;
