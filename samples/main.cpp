bf30558086e077c6b67769bd74d977256beba51a
Johann Anhofer <johann.anhofer@gmail.com>
2013-12-09 17:03:24 +0100

    Fix samples project for CTP Nov 2013 of VC++.

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,4 +1,3 @@
-#include "firebird_driver.h"
 #include "sqlite_driver.h"
 #include "driver_factory.h"
 
@@ -9,7 +8,6 @@ void run_samples();
 int main()
 {
     cpp_db::driver_factory::instance().register_driver("sqlite", []{return cpp_db::sqlite_driver::create();});
-    cpp_db::driver_factory::instance().register_driver("firebird", []{return cpp_db::firebird_driver::create();});
 
 	run_samples();
 
