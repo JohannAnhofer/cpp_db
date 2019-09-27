71b333975a6c0ea1dfc3de5b9d1789013c95b32f
Johann Anhofer <johann.anhofer@meon-medical.com>
2019-09-27 09:35:33 +0200

    Remove postgres test from main test

--- main/main.cpp
+++ main/main.cpp
@@ -4,7 +4,6 @@
 #include "test_app.h"
 #include "test_firebird_class.h"
 #include "test_sqlite_class.h"
-#include "test_postgres_class.h"
 
 BEGIN_DECLARE_TEST_APP([]{})
 	DECLARE_TEST_CLASS(test_test_class)	
@@ -12,5 +11,4 @@ BEGIN_DECLARE_TEST_APP([]{})
     DECLARE_TEST_CLASS(test_cpp_db_class)
     DECLARE_TEST_CLASS(test_sqlite_class)
 	DECLARE_TEST_CLASS(test_firebird_class)
-    DECLARE_TEST_CLASS(test_postgres_class)
 END_DECLARE_TEST_APP([]{})
