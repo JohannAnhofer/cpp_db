fcd327b0662589e8527f088821a938b44bad5272
Johann Anhofer <johann.anhofer@meon-medical.com>
2019-09-27 11:04:55 +0200

    Move firebird tests to a separate executable.

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,6 @@
 #include "test_cpp_db_class.h"
 #include "test_test_class.h"
 #include "test_app.h"
-#include "test_firebird_class.h"
 #include "test_sqlite_class.h"
 
 BEGIN_DECLARE_TEST_APP([]{})
@@ -10,5 +9,4 @@ BEGIN_DECLARE_TEST_APP([]{})
     DECLARE_TEST_CLASS(test_tools_class)
     DECLARE_TEST_CLASS(test_cpp_db_class)
     DECLARE_TEST_CLASS(test_sqlite_class)
-	DECLARE_TEST_CLASS(test_firebird_class)
 END_DECLARE_TEST_APP([]{})
