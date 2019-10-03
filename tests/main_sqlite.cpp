03d2b7cd8a86b491f523117894b17867501b5a6c
Johann Anhofer <johann.anhofer@gmail.com>
2019-10-03 21:30:11 +0200

    Use Boot test for sqlite tests Get rid of proprietary testframework

--- tests/main_sqlite.cpp
+++
@@ -1,6 +0,0 @@
-#include "test_app.h"
-#include "test_sqlite_class.h"
-
-BEGIN_DECLARE_TEST_APP([]{})
-    DECLARE_TEST_CLASS(test_sqlite_class)
-END_DECLARE_TEST_APP([]{})
--- tests/test_sqlite_class.cpp
+++ tests/test_sqlite_class.cpp
@@ -1,4 +1,9 @@
-#include "test_sqlite_class.h"
+#define BOOST_TEST_MODULE cpp_db_postgres
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "connection.h"
 #include "sqlite_driver.h"
 #include "statement.h"
 #include "result.h"
@@ -10,116 +15,122 @@
 
 #include <cmath>
 
-void test_sqlite_class::init_class()
+struct SQLiteTestFixture
 {
-	cpp_db::driver_registry::register_driver("sqlite", [] {return cpp_db::sqlite_driver::create(); });
-    con = std::shared_ptr<cpp_db::connection>(new cpp_db::connection("sqlite"));
-    TEST_FOR_NO_EXCEPTION(con->open(":memory:", cpp_db::no_authentication{}));
+	SQLiteTestFixture()
+	{
+		con = std::shared_ptr<cpp_db::connection>(new cpp_db::connection("sqlite"));
+		BOOST_CHECK_NO_THROW(con->open(":memory:", cpp_db::no_authentication{}));
+	}
+
+	~SQLiteTestFixture()
+	{
+		con.reset();
+	}
+
+	std::shared_ptr<cpp_db::connection> con;
+};
+
+BOOST_FIXTURE_TEST_SUITE(test_cpp_db_sqlite, SQLiteTestFixture)
+
+BOOST_AUTO_TEST_CASE(test_connection)
+{
+    BOOST_CHECK_NO_THROW(cpp_db::statement("create table TEST_TABLE(COL1 INT NOT NULL, COL2 VARCHAR(50));", *con.get()).execute_non_query());
+    BOOST_CHECK_NO_THROW(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(1, 'first')", *con.get()).execute_non_query());
+    BOOST_CHECK_NO_THROW(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(2, 'second')", *con.get()).execute_non_query());
+    BOOST_CHECK_NO_THROW(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(3, 'third')", *con.get()).execute_non_query());
+	BOOST_CHECK_THROW(cpp_db::statement("select COL1, COL2, COL3 from TEST_TABLE;", *con.get()).execute_non_query(), cpp_db::db_exception);
 }
 
-void test_sqlite_class::cleanup_class()
-{
-    con.reset();
-}
-
-void test_sqlite_class::test_connection()
-{
-    TEST_FOR_NO_EXCEPTION(cpp_db::statement("create table TEST_TABLE(COL1 INT NOT NULL, COL2 VARCHAR(50));", *con.get()).execute_non_query());
-    TEST_FOR_NO_EXCEPTION(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(1, 'first')", *con.get()).execute_non_query());
-    TEST_FOR_NO_EXCEPTION(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(2, 'second')", *con.get()).execute_non_query());
-    TEST_FOR_NO_EXCEPTION(cpp_db::statement("insert into TEST_TABLE(COL1, COL2) VALUES(3, 'third')", *con.get()).execute_non_query());
-    TEST_FOR_EXCEPTION(cpp_db::statement("select COL1, COL2, COL3 from TEST_TABLE;", *con.get()).execute_non_query(), cpp_db::db_exception);
-}
-
-void test_sqlite_class::test_statement()
+BOOST_AUTO_TEST_CASE(test_statement)
 {
     cpp_db::statement stmt(*con.get());
-    TEST_FOR_NO_EXCEPTION(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(?, ?)"));
-    TEST_FOR_EXCEPTION(stmt.execute_non_query(), cpp_db::db_exception);
+    BOOST_CHECK_NO_THROW(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(?, ?)"));
+	BOOST_CHECK_THROW(stmt.execute_non_query(), cpp_db::db_exception);
 }
 
-void test_sqlite_class::test_result()
+BOOST_AUTO_TEST_CASE(test_result)
 {
     cpp_db::result r(cpp_db::statement("select COL1, COL2 from TEST_TABLE;", *con.get()).execute());
-    TEST_EQUAL(r.get_column_count(), 2);
-    TEST_EQUAL(r.get_column_name(0), "COL1");
-    TEST_EQUAL(r.get_column_name(1), "COL2");
-    TEST_EQUAL(r.get_column_index("COL1"), 0);
-    TEST_EQUAL(r.get_column_index("COL2"), 1);
-    TEST_VERIFY(!r.is_eof());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value(0)), 1);
-    TEST_EQUAL(cpp_db::value_of<std::string>(r.get_column_value(1)), "first");
-    TEST_FOR_NO_EXCEPTION(r.move_next());
-    TEST_VERIFY(!r.is_eof());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value("COL1")), 2);
-    TEST_EQUAL(cpp_db::value_of<std::string>(r.get_column_value("COL2")), "second");
-    TEST_FOR_NO_EXCEPTION(r.move_next());
-    TEST_VERIFY(!r.is_eof());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value(0)), 3);
-    TEST_EQUAL(cpp_db::value_of<std::string>(r.get_column_value(1)), "third");
-    TEST_FOR_NO_EXCEPTION(r.move_next());
-    TEST_VERIFY(r.is_eof());
+    BOOST_CHECK_EQUAL(r.get_column_count(), 2);
+    BOOST_CHECK_EQUAL(r.get_column_name(0), "COL1");
+    BOOST_CHECK_EQUAL(r.get_column_name(1), "COL2");
+    BOOST_CHECK_EQUAL(r.get_column_index("COL1"), 0);
+    BOOST_CHECK_EQUAL(r.get_column_index("COL2"), 1);
+    BOOST_CHECK(!r.is_eof());
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value(0)), 1);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<std::string>(r.get_column_value(1)), "first");
+    BOOST_CHECK_NO_THROW(r.move_next());
+    BOOST_CHECK(!r.is_eof());
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value("COL1")), 2);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<std::string>(r.get_column_value("COL2")), "second");
+    BOOST_CHECK_NO_THROW(r.move_next());
+    BOOST_CHECK(!r.is_eof());
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(r.get_column_value(0)), 3);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<std::string>(r.get_column_value(1)), "third");
+    BOOST_CHECK_NO_THROW(r.move_next());
+    BOOST_CHECK(r.is_eof());
 
-    TEST_FOR_EXCEPTION(cpp_db::result(cpp_db::statement(*con.get()).execute()), cpp_db::db_exception);
+    BOOST_CHECK_THROW(cpp_db::result(cpp_db::statement(*con.get()).execute()), cpp_db::db_exception);
 }
-void test_sqlite_class::test_parameter()
+BOOST_AUTO_TEST_CASE(test_parameter)
 {
     cpp_db::statement stmt(*con.get());
-    TEST_FOR_NO_EXCEPTION(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(?, ?)"));
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query(4, "four"));
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query(5, "five"));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where (COL1 = 4 and COL2 = 'four') OR (COL1 = 5 and COL2 = 'five')", *con.get()).execute_scalar()), 2);
+    BOOST_CHECK_NO_THROW(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(?, ?)"));
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query(4, "four"));
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query(5, "five"));
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where (COL1 = 4 and COL2 = 'four') OR (COL1 = 5 and COL2 = 'five')", *con.get()).execute_scalar()), 2);
 
-    TEST_FOR_NO_EXCEPTION(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(99, 'Unknown')"));
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query());
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query());
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query());
+    BOOST_CHECK_NO_THROW(stmt.prepare("insert into TEST_TABLE(COL1, COL2) VALUES(99, 'Unknown')"));
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query());
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query());
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query());
 
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where COL1=99 and COL2='Unknown'", *con.get()).execute_scalar()), 3);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where COL1=99 and COL2='Unknown'", *con.get()).execute_scalar()), 3);
 
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where COL1=? and COL2=?", *con.get()).execute_scalar(99, "Unknown")), 3);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE where COL1=? and COL2=?", *con.get()).execute_scalar(99, "Unknown")), 3);
 }
 
-void test_sqlite_class::test_named_parameter()
+BOOST_AUTO_TEST_CASE(test_named_parameter)
 {
     cpp_db::statement stmt("select count(*) from TEST_TABLE where COL1=@COL1 and COL2=@COL2", *con.get());
     cpp_db::parameters params(stmt.get_parameters());
     params.bind(cpp_db::parameter("@COL1", 99));
     params.bind(cpp_db::parameter("@COL2", "Unknown"));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(stmt.execute_scalar()), 3);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(stmt.execute_scalar()), 3);
 }
 
-void test_sqlite_class::test_transaction_commit()
+BOOST_AUTO_TEST_CASE(test_transaction_commit)
 {
     cpp_db::transaction tran(*con.get());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 8);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 8);
 
-    TEST_FOR_NO_EXCEPTION(tran.begin());
+    BOOST_CHECK_NO_THROW(tran.begin());
     cpp_db::statement stmt(*con.get());
-    TEST_FOR_NO_EXCEPTION(stmt.prepare("delete from TEST_TABLE where COL1 = ?"));
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query(99));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
-    TEST_FOR_NO_EXCEPTION(tran.commit());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
+    BOOST_CHECK_NO_THROW(stmt.prepare("delete from TEST_TABLE where COL1 = ?"));
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query(99));
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
+    BOOST_CHECK_NO_THROW(tran.commit());
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
 }
 
-void test_sqlite_class::test_transaction_rollback()
+BOOST_AUTO_TEST_CASE(test_transaction_rollback)
 {
     cpp_db::transaction tran(*con.get());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
 
-    TEST_FOR_NO_EXCEPTION(tran.begin());
+    BOOST_CHECK_NO_THROW(tran.begin());
     cpp_db::statement stmt(*con.get());
-    TEST_FOR_NO_EXCEPTION(stmt.prepare("delete from TEST_TABLE where COL1 > ?"));
-    TEST_FOR_NO_EXCEPTION(stmt.execute_non_query(3));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 3);
-    TEST_FOR_NO_EXCEPTION(tran.rollback());
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
+    BOOST_CHECK_NO_THROW(stmt.prepare("delete from TEST_TABLE where COL1 > ?"));
+    BOOST_CHECK_NO_THROW(stmt.execute_non_query(3));
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 3);
+    BOOST_CHECK_NO_THROW(tran.rollback());
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::statement("select count(*) from TEST_TABLE", *con.get()).execute_scalar()), 5);
 }
 
-void test_sqlite_class::test_execute()
+BOOST_AUTO_TEST_CASE(test_execute)
 {
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 5);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 5);
     cpp_db::result r = cpp_db::execute(*con.get(), "select * from TEST_TABLE where COL1 in (?, ?)", 1, 2);
     int64_t sum = 0;
     std::string names;
@@ -129,19 +140,32 @@ void test_sqlite_class::test_execute()
         names.append(cpp_db::value_of<std::string>(r.get_column_value(1)));
         r.move_next();
     }
-    TEST_EQUAL(sum, 3);
-    TEST_EQUAL(names, "firstsecond");
+    BOOST_CHECK_EQUAL(sum, 3);
+    BOOST_CHECK_EQUAL(names, "firstsecond");
 
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE where COL1 in (?, ?) and COL2 in (?, ?)", 1, 2, "first", "second")), 2);
-    TEST_FOR_NO_EXCEPTION(cpp_db::execute_non_query(*con.get(), "delete from TEST_TABLE where COL1 = ?", 1));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 4);
-    TEST_FOR_NO_EXCEPTION(cpp_db::execute_non_query(*con.get(), "delete from TEST_TABLE"));
-    TEST_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 0);
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE where COL1 in (?, ?) and COL2 in (?, ?)", 1, 2, "first", "second")), 2);
+    BOOST_CHECK_NO_THROW(cpp_db::execute_non_query(*con.get(), "delete from TEST_TABLE where COL1 = ?", 1));
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 4);
+    BOOST_CHECK_NO_THROW(cpp_db::execute_non_query(*con.get(), "delete from TEST_TABLE"));
+    BOOST_CHECK_EQUAL(cpp_db::value_of<int64_t>(cpp_db::execute_scalar(*con.get(), "select count(*) from TEST_TABLE")), 0);
 }
 
-void test_sqlite_class::test_result_empty()
+BOOST_AUTO_TEST_CASE(test_result_emptys)
 {
     cpp_db::result r(cpp_db::statement("select COL1, COL2 from TEST_TABLE where col1 = ?", *con.get()).execute("empty result"));
-    TEST_VERIFY(r.is_eof());
+    BOOST_CHECK(r.is_eof());
 
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+bool init_sqlite_driver()
+{
+	cpp_db::driver_registry::register_driver("sqlite", [] {return cpp_db::sqlite_driver::create(); });
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	return boost::unit_test::unit_test_main(&init_sqlite_driver, argc, argv);
 }
