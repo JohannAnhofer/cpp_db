475887b32644373537085d5562ae1f29e74aca5d
Johann Anhofer <johann.anhofer@meon-medical.com>
2019-10-03 10:55:29 +0200

    Fix test for using boost test

--- tests/test_test_class.cpp
+++ tests/test_test_class.cpp
@@ -1,4 +1,3 @@
-#define BOOST_TEST_MODULE tests
 #include <boost/test/unit_test.hpp>
 
 #include <stdexcept>
--- tests/test_tools_class.cpp
+++ tests/test_tools_class.cpp
@@ -1,4 +1,3 @@
-#define BOOST_TEST_MODULE tools
 #include <boost/test/unit_test.hpp>
 
 #include "coalesce.h"
